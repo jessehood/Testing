97eef45fbd544cec7fff6be548add5e0bdfc7705
Jesse Hood <jessenoob@gmail.com>
2018-01-29 18:04:17 -0500

    solved basic-javascript tests

--- basic-javascript/tests/project-1.test.js
+++ basic-javascript/tests/project-1.test.js
@@ -1,4 +1,5 @@
-const assert = require('chai').assert;
+/* eslint-disable prefer-destructuring */
+const { assert, expect } = require('chai');
 const funcs = require('../src/project-1');
 
 // we've gone ahead and gotten a start here for you,
@@ -11,177 +12,320 @@ const funcs = require('../src/project-1');
 describe('Project-1 Functions', () => {
   describe('`multiplyByTen`', () => {
     it('should be a function', () => {
-      const multiplyByTen = funcs.multiplyByTen;
-      assert.typeOf(multiplyByTen, 'object');
+      const { multiplyByTen } = funcs;
+      assert.typeOf(multiplyByTen, 'function');
     });
     // begin here
+    it('should return 100 when passed 10', () => {
+      const { multiplyByTen } = funcs;
+      expect(multiplyByTen(10)).to.equal(100);
+    });
+    it('should return a number', () => {
+      expect(funcs.multiplyByTen(10)).to.be.a('number');
+    });
   });
 
   describe('`subtractFive`', () => {
     it('should be a function', () => {
       const subtractFive = funcs.subtractFive;
-      assert.typeOf(subtractFive, 'array');
+      assert.typeOf(subtractFive, 'function');
+    });
+    it('should return 0 when passed 5', () => {
+      expect(funcs.subtractFive(5)).to.equal(0);
+    });
+    it('should return a number', () => {
+      expect(funcs.subtractFive(5)).to.be.a('number');
     });
   });
 
   describe('`areSameLength`', () => {
     it('should be a function', () => {
       const areSameLength = funcs.areSameLength;
-      assert.typeOf(areSameLength, 'number');
+      assert.typeOf(areSameLength, 'function');
+    });
+    it('should return false when given two unequal strings', () => {
+      const { areSameLength } = funcs;
+      expect(funcs.areSameLength('milk', 'bread')).to.equal(false);
     });
   });
 
   describe('`areEqual`', () => {
     it('should be a function', () => {
       const areEqual = funcs.areEqual;
-      assert.typeOf(areEqual, 'number');
+      assert.typeOf(areEqual, 'function');
+    });
+    it('should return true if both numbers passed are the same', () => {
+      expect(funcs.areEqual(1, 1)).to.equal(true);
+    });
+    it('should return false if the numbers are different', () => {
+      expect(funcs.areEqual(1, 2)).to.equal(false);
     });
   });
 
   describe('`lessThanNinety`', () => {
     it('should be a function', () => {
       const lessThanNinety = funcs.lessThanNinety;
-      assert.typeOf(lessThanNinety, 'number');
+      assert.typeOf(lessThanNinety, 'function');
+    });
+    it('should return true if num passed is less than 90', () => {
+      expect(funcs.lessThanNinety(1)).to.equal(true);
+    });
+    it('should return false if num passed is greater than 90', () => {
+      expect(funcs.lessThanNinety(91)).to.equal(false);
     });
   });
 
   describe('`greaterThanFifty`', () => {
     it('should be a function', () => {
       const greaterThanFifty = funcs.greaterThanFifty;
-      assert.typeOf(greaterThanFifty, 'number');
+      assert.typeOf(greaterThanFifty, 'function');
+    });
+    it('should return true if num passed is greater than 50', () => {
+      expect(funcs.greaterThanFifty(51)).to.equal(true);
+    });
+    it('should return false if num passed is less than 50', () => {
+      expect(funcs.greaterThanFifty(49)).to.equal(false);
     });
   });
 
   describe('`add`', () => {
     it('should be a function', () => {
       const add = funcs.add;
-      assert.typeOf(add, 'number');
+      assert.typeOf(add, 'function');
+    });
+    it('should return 10 when passed 5 and 5', () => {
+      expect(funcs.add(5, 5)).to.equal(10);
+    });
+    it('should return -7 when passed -3 and -4', () => {
+      expect(funcs.add(-3, -4)).to.equal(-7);
     });
   });
 
   describe('`subtract`', () => {
     it('should be a function', () => {
       const subtract = funcs.subtract;
-      assert.typeOf(subtract, 'number');
+      assert.typeOf(subtract, 'function');
+    });
+    it('should return 5 when given 10 and 5', () => {
+      expect(funcs.subtract(10, 5)).to.equal(5);
+    });
+    it('should return -8 when given 10 and 18', () => {
+      expect(funcs.subtract(10, 18)).to.equal(-8);
     });
   });
 
   describe('`divide`', () => {
     it('should be a function', () => {
       const divide = funcs.divide;
-      assert.typeOf(divide, 'number');
+      assert.typeOf(divide, 'function');
+    });
+    it('should equal 10 when given 100 and 10', () => {
+      expect(funcs.divide(100, 10)).to.equal(10);
+    });
+    it('should equal 1.2 when given -6 and -5', () => {
+      expect(funcs.divide(-6, -5)).to.equal(1.2);
     });
   });
 
   describe('`multiply`', () => {
     it('should be a function', () => {
       const multiply = funcs.multiply;
-      assert.typeOf(multiply, 'number');
+      assert.typeOf(multiply, 'function');
+    });
+    it('should equal 70 when given 10 and 7 ', () => {
+      expect(funcs.multiply(10, 7)).to.equal(70);
+    });
+    it('should equal 64 when given -8 and -8', () => {
+      expect(funcs.multiply(-8, -8)).to.equal(64);
     });
   });
 
   describe('`getRemainder`', () => {
     it('should be a function', () => {
       const getRemainder = funcs.getRemainder;
-      assert.typeOf(getRemainder, 'number');
+      assert.typeOf(getRemainder, 'function');
+    });
+    it('should equal 1 when given 10 and 3', () => {
+      expect(funcs.getRemainder(10, 3)).to.equal(1);
     });
   });
 
   describe('`isEven`', () => {
     it('should be a function', () => {
       const isEven = funcs.isEven;
-      assert.typeOf(isEven, 'number');
+      assert.typeOf(isEven, 'function');
+    });
+    it('should equal true when given 2', () => {
+      expect(funcs.isEven(2)).to.equal(true);
+    });
+    it('should equal false when given 5', () => {
+      expect(funcs.isEven(5)).to.equal(false);
     });
   });
 
   describe('`isOdd`', () => {
     it('should be a function', () => {
       const isOdd = funcs.isOdd;
-      assert.typeOf(isOdd, 'number');
+      assert.typeOf(isOdd, 'function');
+    });
+    it('should equal true when given 7', () => {
+      expect(funcs.isOdd(7)).to.equal(true);
+    });
+    it('should equal false when given 10', () => {
+      expect(funcs.isOdd(10)).to.equal(false);
     });
   });
 
   describe('`square`', () => {
     it('should be a function', () => {
       const square = funcs.square;
-      assert.typeOf(square, 'object');
+      assert.typeOf(square, 'function');
+    });
+    it('should return 9 when given 3', () => {
+      expect(funcs.square(3)).to.equal(9);
+    });
+    it('should return 25 when given -5', () => {
+      expect(funcs.square(-5)).to.equal(25);
     });
   });
 
   describe('`cube`', () => {
     it('should be a function', () => {
       const cube = funcs.cube;
-      assert.typeOf(cube, 'object');
+      assert.typeOf(cube, 'function');
+    });
+    it('should return 27 when given 3', () => {
+      expect(funcs.cube(3)).to.equal(27);
+    });
+    it('should return -125 when given -55', () => {
+      expect(funcs.cube(-5)).to.equal(-125);
     });
   });
 
   describe('`raiseToPower`', () => {
     it('should be a function', () => {
       const raiseToPower = funcs.raiseToPower;
-      assert.typeOf(raiseToPower, 'object');
+      assert.typeOf(raiseToPower, 'function');
+    });
+    it('should equal 64 when given 4 and 3', () => {
+      expect(funcs.raiseToPower(4, 3)).to.equal(64);
+    });
+    it('should equal -32 when given -2 and 5', () => {
+      expect(funcs.raiseToPower(-2, 5)).to.equal(-32);
     });
   });
 
   describe('`roundNumber`', () => {
     it('should be a function', () => {
       const roundNumber = funcs.roundNumber;
-      assert.typeOf(roundNumber, 'object');
+      assert.typeOf(roundNumber, 'function');
+    });
+    it('should equal 7 when given 6.584', () => {
+      expect(funcs.roundNumber(6.584)).to.equal(7);
+    });
+    it('should equal 9 when given 9.49999', () => {
+      expect(funcs.roundNumber(9.49999)).to.equal(9);
     });
   });
 
   describe('`roundUp`', () => {
     it('should be a function', () => {
       const roundUp = funcs.roundUp;
-      assert.typeOf(roundUp, 'object');
+      assert.typeOf(roundUp, 'function');
+    });
+    it('should equal 2 when given 1.0001', () => {
+      expect(funcs.roundUp(1.0001)).to.equal(2);
+    });
+    it('should equal 5 when given 5', () => {
+      expect(funcs.roundUp(5)).to.equal(5);
     });
   });
 
   describe('`addExclamationPoint`', () => {
     it('should be a function', () => {
       const addExclamationPoint = funcs.addExclamationPoint;
-      assert.typeOf(addExclamationPoint, 'string');
+      assert.typeOf(addExclamationPoint, 'function');
+    });
+    it('should equal "hello!" when given "hello"', () => {
+      expect(funcs.addExclamationPoint('hello')).to.equal('hello!');
+    });
+    it('should equal ! when given an empty string', () => {
+      expect(funcs.addExclamationPoint('')).to.equal('!');
     });
   });
 
   describe('`combineNames`', () => {
     it('should be a function', () => {
       const combineNames = funcs.combineNames;
-      assert.typeOf(combineNames, 'string');
+      assert.typeOf(combineNames, 'function');
+    });
+    it('should equal "Kanye West" when given "Kanye" and "West"', () => {
+      expect(funcs.combineNames('Kanye', 'West')).to.equal('Kanye West');
+    });
+    it('should equal "John Doe" when given "John" and "Doe', () => {
+      expect(funcs.combineNames('John', 'Doe')).to.equal('John Doe');
     });
   });
 
   describe('`getGreeting`', () => {
     it('should be a function', () => {
       const getGreeting = funcs.getGreeting;
-      assert.typeOf(getGreeting, 'string');
+      assert.typeOf(getGreeting, 'function');
+    });
+    it('should return "Hello World!" when given "World"', () => {
+      expect(funcs.getGreeting('World')).to.equal('Hello World!');
     });
   });
 
   describe('`getRectangleArea`', () => {
     it('should be a function', () => {
       const getRectangleArea = funcs.getRectangleArea;
-      assert.typeOf(getRectangleArea, 'string');
+      assert.typeOf(getRectangleArea, 'function');
+    });
+    it('should equal 24 when given 6 and 4', () => {
+      expect(funcs.getRectangleArea(6, 4)).to.equal(24);
+    });
+    it('should equal 0 when given 7 and 0', () => {
+      expect(funcs.getRectangleArea(7, 0)).to.equal(0);
     });
   });
 
   describe('`getTriangleArea`', () => {
     it('should be a function', () => {
       const getTriangleArea = funcs.getTriangleArea;
-      assert.typeOf(getTriangleArea, 'string');
+      assert.typeOf(getTriangleArea, 'function');
+    });
+    it('should equal 25 when given a 10 base and 5 height', () => {
+      expect(funcs.getTriangleArea(10, 5)).to.equal(25);
+    });
+    it('should equal 0 when given a 0 base and 99 height', () => {
+      expect(funcs.getTriangleArea(0, 99)).to.equal(0);
     });
   });
 
   describe('`getCircleArea`', () => {
     it('should be a function', () => {
       const getCircleArea = funcs.getCircleArea;
-      assert.typeOf(getCircleArea, 'string');
+      assert.typeOf(getCircleArea, 'function');
+    });
+    it('should approximately equal 12.56 when given 2', () => {
+      assert.approximately(funcs.getCircleArea(2), 12.56, 0.01);
+    });
+    it('should equal 0 when given 0', () => {
+      expect(funcs.getCircleArea(0)).to.equal(0);
     });
   });
 
   describe('`getRectangularPrismVolume`', () => {
     it('should be a function', () => {
       const getRectangularPrismVolume = funcs.getRectangularPrismVolume;
-      assert.typeOf(getRectangularPrismVolume, 'string');
+      assert.typeOf(getRectangularPrismVolume, 'function');
+    });
+    it('should equal 50 when given a width of 5, height of 2, and length of 5', () => {
+      expect(funcs.getRectangularPrismVolume(5, 2, 5)).to.equal(50);
+    });
+    it('should equal 0 when given a width of 0, a height of 99, and length of 777', () => {
+      expect(funcs.getRectangularPrismVolume(0, 99, 777)).to.equal(0);
     });
   });
 });
--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -1,4 +1,281 @@
-const assert = require('chai').assert;
+const { assert } = require('chai');
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('getBiggest', () => {
+  it('should be a function', () => {
+    const { getBiggest } = funcs;
+    assert.typeOf(getBiggest, 'function');
+  });
+  it('should return 100 given 100 and 99', () => {
+    const { getBiggest } = funcs;
+    assert.equal(getBiggest(100, 99), 100);
+  });
+  it('should return 10 given -50 and 10', () => {
+    const { getBiggest } = funcs;
+    assert.equal(getBiggest(-50, 10), 10);
+  });
+});
+
+describe('greeting', () => {
+  it('should be a function', () => {
+    const { greeting } = funcs;
+    assert.typeOf(greeting, 'function');
+  });
+  it('should return "Hola!" when given "Spanish"', () => {
+    const { greeting } = funcs;
+    assert.equal(greeting('Spanish'), 'Hola!');
+  });
+  it('should return "Hello!" when given undefined', () => {
+    const { greeting } = funcs;
+    assert.equal(greeting(undefined), 'Hello!');
+  });
+});
+
+describe('isTenOrFive', () => {
+  it('should be a function', () => {
+    const { isTenOrFive } = funcs;
+    assert.typeOf(isTenOrFive, 'function');
+  });
+  it('should return true if given 5 or 10', () => {
+    const { isTenOrFive } = funcs;
+    assert.isTrue(isTenOrFive(10));
+    assert.isTrue(isTenOrFive(5));
+  });
+  it('should return false if given a string of the number 5', () => {
+    const { isTenOrFive } = funcs;
+    assert.isFalse(isTenOrFive('5'));
+  });
+});
+
+describe('isInRange', () => {
+  it('should be a function', () => {
+    const { isInRange } = funcs;
+    assert.typeOf(isInRange, 'function');
+  });
+  it('should return true when given 30', () => {
+    const { isInRange } = funcs;
+    assert.isTrue(isInRange(30));
+  });
+  it('should return false when given 60', () => {
+    const { isInRange } = funcs;
+    assert.isFalse(isInRange(60));
+  });
+});
+
+describe('isInteger', () => {
+  it('should be a function', () => {
+    const { isInteger } = funcs;
+    assert.typeOf(isInteger, 'function');
+  });
+  it('should return true when given 10', () => {
+    const { isInteger } = funcs;
+    assert.isTrue(isInteger(10));
+  });
+  it('should return false when given 2.22', () => {
+    const { isInteger } = funcs;
+    assert.isFalse(isInteger(2.22));
+  });
+});
+
+describe('fizzBuzz', () => {
+  it('should be a function', () => {
+    const { fizzBuzz } = funcs;
+    assert.typeOf(fizzBuzz, 'function');
+  });
+  it('should return fizzbuzz when given 15', () => {
+    const { fizzBuzz } = funcs;
+    assert.equal(fizzBuzz(15), 'fizzbuzz');
+  });
+  it('should return buzz when given 10', () => {
+    const { fizzBuzz } = funcs;
+    assert.equal(fizzBuzz(10), 'buzz');
+  });
+  it('should return fizz when given 9', () => {
+    const { fizzBuzz } = funcs;
+    assert.equal(fizzBuzz(9), 'fizz');
+  });
+});
+
+describe('isPrime', () => {
+  it('should be a function', () => {
+    const { isPrime } = funcs;
+    assert.typeOf(isPrime, 'function');
+  });
+  it('should return false when given 1', () => {
+    const { isPrime } = funcs;
+    assert.isFalse(isPrime(1));
+  });
+  it('should return true when given 7', () => {
+    const { isPrime } = funcs;
+    assert.isTrue(isPrime(7));
+  });
+});
+
+describe('returnFirst', () => {
+  it('should be a function', () => {
+    const { returnFirst } = funcs;
+    assert.typeOf(returnFirst, 'function');
+  });
+  it('should return 1 when given [1,2,3]', () => {
+    const { returnFirst } = funcs;
+    assert.equal(returnFirst([1, 2, 3]), 1);
+  });
+  it('should return undefined when given an empty array', () => {
+    const { returnFirst } = funcs;
+    assert.equal(returnFirst([]), undefined);
+  });
+});
+
+describe('returnLast', () => {
+  it('should be a function', () => {
+    const { returnLast } = funcs;
+    assert.typeOf(returnLast, 'function');
+  });
+
+  it('should return 3 when given [1,2,3]', () => {
+    const { returnLast } = funcs;
+    assert.equal(returnLast([1, 2, 3]), 3);
+  });
+
+  it('should return undefined when given an empty array', () => {
+    const { returnLast } = funcs;
+    assert.equal(returnLast([]), undefined);
+  });
+});
+
+describe('getArrayLength', () => {
+  it('should be a function', () => {
+    const { getArrayLength } = funcs;
+    assert.typeOf(getArrayLength, 'function');
+  });
+
+  it('should return 3 when given an array ["a","b","c"]', () => {
+    const { getArrayLength } = funcs;
+    assert.equal(getArrayLength(['a', 'b', 'c']), 3);
+  });
+  
+  it('should return 0 when given an empty array', () => {
+    const { getArrayLength } = funcs;
+    assert.equal(getArrayLength([]), 0);
+  });
+});
+
+describe('incrementByOne', () => {
+  it('should be a function', () => {
+    const { incrementByOne } = funcs;
+    assert.typeOf(incrementByOne, 'function');
+  });
+  
+  it('should return [2,3,4] when given [1,2,3]', () => {
+    const { incrementByOne } = funcs;
+    assert.deepEqual(incrementByOne([1, 2, 3]), [2, 3, 4]);
+  });
+
+  it('should return an empty array given an empty array', () => {
+    const { incrementByOne } = funcs;
+    assert.deepEqual(incrementByOne([]), []);
+  });
+});
+
+describe('addItemToArray', () => {
+  it('should be a function', () => {
+    const { addItemToArray } = funcs;
+    assert.typeOf(addItemToArray, 'function');
+  });
+  it('should return [1, 2, 3, 4] when given [1,2,3] and 4', () => {
+    const { addItemToArray } = funcs;
+    assert.deepEqual(addItemToArray([1, 2, 3], 4), [1, 2, 3, 4]);
+  });
+  it('should return ["a"] when given an empty array and "a"', () => {
+    const { addItemToArray } = funcs;
+    assert.deepEqual(addItemToArray([], 'a'), ['a']);
+  });
+});
+
+describe('addItemToFront', () => {
+  it('should be a function', () => {
+    const { addItemToFront } = funcs;
+    assert.typeOf(addItemToFront, 'function');
+  });
+  it('should return [4, 1, 2, 3] when given [1, 2, 3] and 4', () => {
+    const { addItemToFront } = funcs;
+    assert.deepEqual(addItemToFront([1, 2, 3], 4), [4, 1, 2 ,3]);
+  });
+  it('should return ["a"] when given an empty array and "a"', () => {
+    const { addItemToFront } = funcs;
+    assert.deepEqual(addItemToFront([], 'a'), ['a']);
+  });
+});
+
+describe('wordsToSentence', () => {
+  it('should be a function', () => {
+    const { wordsToSentence } = funcs;
+    assert.typeOf(wordsToSentence, 'function');
+  });
+  it('should return "My name is Jeff" when given ["My","name","is","Jeff"]', () => {
+    const { wordsToSentence } = funcs;
+    assert.equal(wordsToSentence(['My', 'name', 'is', 'Jeff']), 'My name is Jeff');
+  });
+});
+
+describe('contains', () => {
+  it('should be a function', () => {
+    const { contains } = funcs;
+    assert.typeOf(contains, 'function');
+  });
+  it('should return true when searching an array of [1, 2, 3] for 2', () => {
+    const { contains } = funcs;
+    assert.isTrue(contains([1, 2, 3], 2));
+  });
+  it('should return false when searching an array of[1, 2, 3] for 4', () => {
+    const { contains } = funcs;
+    assert.isFalse(contains([1, 2, 3], 4));
+  });
+});
+
+describe('addNumbers', () => {
+  it('should be a function', () => {
+    const { addNumbers } = funcs;
+    assert.typeOf(addNumbers, 'function');
+  });
+  it('should return 6 when given [1, 2, 3]', () => {
+    const { addNumbers } = funcs;
+    assert.equal(addNumbers([1, 2, 3]), 6);
+  });
+  it('should return 5 when given [5]', () => {
+    const { addNumbers } = funcs;
+    assert.equal(addNumbers([5]), 5);
+  });
+});
+
+describe('averageTestScore', () => {
+  it('should be a function', () => {
+    const { averageTestScore } = funcs;
+    assert.typeOf(averageTestScore, 'function');
+  });
+  it('should return 80 when given [100, 80, 60]', () => {
+    const { averageTestScore } = funcs;
+    assert.equal(averageTestScore([100, 80, 60]), 80);
+  });
+  it('should return NaN when given an empty array', () => {
+    const { averageTestScore } = funcs;
+    assert.isNaN(averageTestScore([]));
+  });
+});
+
+describe('largestNumber', () => {
+  it('should be a function', () => {
+    const { largestNumber } = funcs;
+    assert.typeOf(largestNumber, 'function');
+  });
+  it('should return 3 given an array of [1, 2, 3]', () => {
+    const { largestNumber } = funcs;
+    assert.equal(largestNumber([1, 2, 3]), 3);
+  });
+  it('should return 100 given an array of [100]', () => {
+    const { largestNumber } = funcs;
+    assert.equal(largestNumber([100]), 100);
+  });
+});
+
