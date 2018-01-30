007091cb9f045b0d75de32c80094d3ca1d1a7de0
Jesse Hood <jessenoob@gmail.com>
2018-01-29 19:22:37 -0500

    solved objects and most of arrays

--- advanced-javascript/tests/arrays.test.js
+++ advanced-javascript/tests/arrays.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
@@ -13,48 +14,106 @@ chai.use(sinonChai);
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 // hint 3. - if the function you're testing requires a * callback *, make sure you use a spy like sinon
+let data = [1, 2, 3];
+afterEach(() => {
+  data = [1, 2, 3];
+});
 
 describe('Arrays', () => {
   describe('`each`', () => {
     it('should be a function', () => {
       const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+      expect(each).to.be.a('function');
     });
     // begin here
+    it('should iterate over all elements of an array', () => {
+      const { each } = arrayFunctions;
+      const callback = sinon.spy();
+      each(data, callback);
+      expect(callback.callCount).to.equal(3);
+    });
+    it('should apply a callback to all elements of an array', () => {
+      const { each } = arrayFunctions;
+      const cbResult = [];
+      const callback = (x, i) => cbResult[i] = (x + 1);
+      each(data, callback);
+      expect(cbResult).to.eql([2, 3, 4]);
+    });
   });
 
   describe('`map`', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.a('function');
+    });
+    it('should iterate over all elements of an array', () => {
+      it('should iterate over all elements of an array', () => {
+        const { map } = arrayFunctions;
+        const callback = sinon.spy();
+        map(data, callback);
+        expect(callback.callCount).to.equal(3);
+      });
+    });
+    it('should apply a callback to all elements of an array and return a new array', () => {
+      const { map } = arrayFunctions;
+      const callback = x => x + 1;
+      const cbResult = map(data, callback);
+      expect(cbResult).to.eql([2, 3, 4]);
     });
   });
 
   describe('`reduce`', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
+    });
+    it.skip('should iterate over all elements of an array', () => {
+      const { reduce } = arrayFunctions;
+      const callback = (acc, next) => acc + next;
+      const spyCb = sinon.spy(callback);
+      reduce(data, spyCb);
+      expect(spyCb.callCount).to.equal(3);
     });
   });
 
   describe('`find`', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
-      expect(find).to.be.an('array');
+      expect(find).to.be.a('function');
+    });
+    it('should apply a callback to all elements of an array if not found', () => {
+      const { find } = arrayFunctions;
+      const callback = x => x === 'abc';
+      const spyCb = sinon.spy(callback);
+      find(data, spyCb);
+      expect(spyCb.callCount).to.equal(3);
+    });
+    it('should find an element and return it based on the callback', () => {
+      const { find } = arrayFunctions;
+      const callback = x => x === 2;
+      expect(find(data, callback)).to.equal(2);
     });
   });
 
   describe('`filter`', () => {
     it('should be a function', () => {
       const filter = arrayFunctions.filter;
-      expect(filter).to.be.a('null');
+      expect(filter).to.be.a('function');
+    });
+    it.skip('should apply a callback to all elements of an array', () => {
+      const { filter } = arrayFunctions;
+      const callback = x => x !== 1;
+      const spyCb = sinon.spy(callback);
+      filter(data, spyCb);
+      expect(spyCb.callCount).to.equal(3);
     });
   });
 
   describe('`flatten`', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
-      expect(flatten).to.be.a('promise');
+      expect(flatten).to.be.a('function');
     });
+    it.skip('');
   });
 });
--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,4 +1,122 @@
-const assert = require('chai').expect;
+const { assert, expect } = require('chai');
 const objectFunctions = require('../src/objects');
+const sinon = require('sinon');
 
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+afterEach(() => {
+  obj = {
+    a: 1,
+    b: 2,
+    c: 3
+  };
+});
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Objects', () => {
+  describe('keys', () => {
+    it('should be a function', () => {
+      const { keys } = objectFunctions;
+      assert.isFunction(keys);
+    });
+    it('should return an array of keys from an object', () => {
+      const { keys } = objectFunctions;
+      assert.deepEqual(keys(obj), ['a', 'b', 'c']);
+    });
+    it('should return an empty array for an empty object', () => {
+      const { keys } = objectFunctions;
+      assert.deepEqual(keys({}), []);
+    });
+  });
+  describe('values', () => {
+    it('should be a function', () => {
+      const { values } = objectFunctions;
+      assert.isFunction(values);
+    });
+    it('should return an array of values from an object', () => {
+      const { values } = objectFunctions;
+      assert.deepEqual(values(obj), [1, 2, 3]);
+    });
+    it('should return an empty array for an empty object', () => {
+      const { values } = objectFunctions;
+      assert.deepEqual(values({}), []);
+    });
+  });
+  describe('mapObject', () => {
+    it('should be a function', () => {
+      const { mapObject } = objectFunctions;
+      assert.isFunction(mapObject);
+    });
+    it('should iterate over all elements of an object', () => {
+      const { mapObject } = objectFunctions;
+      const callback = sinon.spy();
+      mapObject(obj, callback);
+      expect(callback.callCount).to.equal(3);
+    });
+    it('should map over an object and return a new object', () => {
+      const { mapObject } = objectFunctions;
+      const callback = x => x + 1;
+      const mappedObj = mapObject(obj, callback);
+      const expectedObj = {
+        a: 2,
+        b: 3,
+        c: 4
+      };
+      assert.deepEqual(mappedObj, expectedObj);
+    });
+  });
+  describe('pairs', () => {
+    it('should be a function', () => {
+      const { pairs } = objectFunctions;
+      assert.isFunction(pairs);
+    });
+    it('should return an array of key/value pairs', () => {
+      const { pairs } = objectFunctions;
+      const pairsResult = pairs(obj);
+      const expectedResult = [['a', 1], ['b', 2], ['c', 3]];
+      assert.deepEqual(pairsResult, expectedResult);
+    });
+    it('should return an empty array for an empty object', () => {
+      const { pairs } = objectFunctions;
+      assert.deepEqual(pairs({}), []);
+    });
+  });
+  describe('invert', () => {
+    it('should be a function', () => {
+      const { invert } = objectFunctions;
+      assert.isFunction(invert);
+    });
+    it('should return an inverted object given an object', () => {
+      const { invert } = objectFunctions;
+      const invertedObj = invert(obj);
+      const expectedResult = {
+        1: 'a',
+        2: 'b',
+        3: 'c'
+      };
+      assert.deepEqual(invertedObj, expectedResult);
+    });
+    it('should return an empty object for an empty object', () => {
+      const { invert } = objectFunctions;
+      assert.deepEqual(invert({}), {});
+    });
+  });
+  describe('defaults', () => {
+    it('should be a function', () => {
+      const { defaults } = objectFunctions;
+      assert.isFunction(defaults);
+    });
+    it('should return an object with defaults applied', () => {
+      const { defaults } = objectFunctions;
+      const defaultProps = { c: 'asdf', d: 4 };
+      const defaultObj = defaults(obj, defaultProps);
+      assert.deepEqual(obj, defaultObj);
+    });
+    it('should return an object given an empty object and an empty default object', () => {
+      const { defaults } = objectFunctions;
+      assert.deepEqual(defaults({}, {}), {});
+    });
+  });
+});
